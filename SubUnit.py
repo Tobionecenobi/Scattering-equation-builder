3c2cb507b152de7f785c8069a6e3ee329ba761b7
Tobionecenobi <45657039+Tobionecenobi@users.noreply.github.com>
2019-04-04 13:23:33 +0200

    Add files via upload

---
+++ SubUnit.py
@@ -0,0 +1,28 @@
+class SubUnit:
+    id = "";
+    referencepoints = {};
+    variable = {};
+
+    # returns the formfactor of the sub unit
+    def formfactor(self, parameter_list):
+        pass
+        return;
+
+    # returns the formfacetor ampltude of the sub unit
+    def formfactorAmplitude()
+        pass
+        return
+
+    # returns the phasefactor of the amplitude 
+    def phasefactor()
+        pass
+        return
+
+    #constructor: Contructs sub unit with an id, its referencepoints its varialbles.
+        def __init__(id,referencepoints, variable):
+        self.id = id;
+        self.referencepoints = referencepoints;
+        self.variable = variable; 
+
+    
+    
