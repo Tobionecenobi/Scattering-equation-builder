3ad2cca3101b3e4cd0d5abeb4015ddf79e82342c
Tobionecenobi <45657039+Tobionecenobi@users.noreply.github.com>
2019-04-04 13:23:04 +0200

    Add files via upload

---
+++ Structure.py
@@ -0,0 +1,35 @@
+class Structure:
+
+    #creates an empty table that can store sub units
+        
+    #creates an empty table that can store information of which subunits is linked
+
+    #creates an empty table that can store information of paths
+
+
+
+
+    #creates sub unit using the subunit class, put that subunit into the subunit table
+        def create(subunit()):
+            pass
+            return;
+
+    #links sub units refenerence point together 
+        def link():
+            pass
+            return;
+
+    #returns the formfactor of the structure build
+        def formfactor():
+            pass
+            return;
+    
+    #returns the formfactor amplitude of the structure build
+        def formfactorAmplitude():
+            pass
+            return;
+
+    #returns the phasefactor of the structure build
+        def phasefactor():
+            pass
+            return;
