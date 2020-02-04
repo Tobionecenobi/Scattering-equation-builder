1012f1b3548e67e03b2f022bb073a047e3142e05
Tobionecenobi <45657039+Tobionecenobi@users.noreply.github.com>
2020-02-04 13:13:47 +0100

    Code updated with code on my computer

--- Structure.py
+++ Structure.py
@@ -1,35 +1,41 @@
+    #takes a file/something as instructions how to build the structure
 class Structure:
+    #Constructor that takes instructions and construct subunit table and linked table
+    def __init__( self , typ , Aeq , Feq , PHIeq , name , refpoints):
+    #Array "subunit_array" that can store subunits
+    def subunitArray():
+        subunitArray = []
+        return subunitArray
+    #array "link_table" that can store information of which subunits is linked
+    def linkArray(refencepointOne, referencepointTwo)
+        return referencepointOne + referencepointTwo
 
-    #creates an empty table that can store sub units
-        
-    #creates an empty table that can store information of which subunits is linked
-
-    #creates an empty table that can store information of paths
+    #table "path_table" that can store information of paths - OPTIONAL
 
 
 
-
-    #creates sub unit using the subunit class, put that subunit into the subunit table
-        def create(subunit()):
+    #creates subunit using the subunit class, put that subunit into the subunit table
+        def create():
+            # make a switch for every kind/geometry of subunit make the switch choose object
             pass
-            return;
+            return
 
     #links sub units refenerence point together 
         def link():
             pass
-            return;
+            return
 
     #returns the formfactor of the structure build
         def formfactor():
             pass
-            return;
+            return
     
     #returns the formfactor amplitude of the structure build
         def formfactorAmplitude():
             pass
-            return;
+            return
 
     #returns the phasefactor of the structure build
         def phasefactor():
             pass
-            return;
+            return
