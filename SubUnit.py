be1a8d01a02433cd127f5c4f4c0dbb7cc57ced1e
Tobionecenobi <45657039+Tobionecenobi@users.noreply.github.com>
2020-02-04 13:18:18 +0100

    uploadet updated code from computer

--- SubUnit.py
+++ SubUnit.py
@@ -1,28 +1,53 @@
+class subunit
+import math
+# returns the formfacetor amplitude of the sub unit as a function, a char and latex-string
+def formfactorAmplitude(typ, name , Aeq ):
+     A = Aeq
+     Asymbol = "A" + name
+     Alatex = "A" + "_" + "{" + name + "}"
+     Alist = {A,Asymbol, Alatex}
+     return Alist
+
+# returns the formfactor of the sub unit as a function, a char and latex-string
+def formfactor(typ, name , Feq ):
+    F = Feq
+    Fsymbol = "F" + name
+    Flatex = "F"  + "_" + "{" + name + "}"
+    Flist = {F,Fsymbol, Flatex}
+    return Flist
+
+
+# returns the phasefactor of the amplitude as a function, a char and latex-string
+def phasefactor(typ, name, PHIeq):
+    PHI = PHIeq 
+    PHIsymbol = "PHI" + name
+    PHIlatex = "\Phi" + "_" + "{" + name + "}"
+    PHIlist = {PHI,PHIsymbol, PHIlatex}
+    return PHIlist
+
 class SubUnit:
-    id = "";
-    referencepoints = {};
-    variable = {};
-
-    # returns the formfactor of the sub unit
-    def formfactor(self, parameter_list):
-        pass
-        return;
-
-    # returns the formfacetor ampltude of the sub unit
-    def formfactorAmplitude()
-        pass
-        return
-
-    # returns the phasefactor of the amplitude 
-    def phasefactor()
-        pass
-        return
 
     #constructor: Contructs sub unit with an id, its referencepoints its varialbles.
-        def __init__(id,referencepoints, variable):
-        self.id = id;
-        self.referencepoints = referencepoints;
-        self.variable = variable; 
+    def __init__( self , typ , Aeq , Feq , PHIeq , name , refpoints):
+        ID = name
+        referencepoints = refpoints
+        variable = var
+        formfactor(typ, ID , Feq)
+        formfactorAmplitude(typ, ID, Aeq )
+        phasefactor(typ, ID, PHIeq )
 
-    
-    
+    #Main functions runs the program
+if __name__ == "__main__":
+        import Rod
+        ID = Rod.ID
+        referencepoints = Rod.referencepoints()
+        typ = Rod.typ()
+        Aeq = Rod.Aeq()
+        Feq = Rod.Feq()
+        PHIeq = Rod.PHIeq()
+        print( formfactorAmplitude( typ , ID , Aeq ) )
+        print( formfactor( typ, ID , Feq ) )
+        print( phasefactor( typ, ID, PHIeq ) )
+        SubUnit(typ, Aeq , Feq, PHIeq , ID , referencepoints, variable)
+
+   
